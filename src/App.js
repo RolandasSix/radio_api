ffbb8faa432e03388aac990c73305e2a8480ea31
Rol <rolis29@mail.com>
2022-05-08 21:17:10 +0300

    fix

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Radio from "./Radio";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Radio Player</h1>
+      <h2>Choose your favorite style of music</h2>
+      <Radio />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
-const root = createRoot(container); 
-root.render(
-  <React.StrictMode>
- <App />
- </React.StrictMode>,
-);
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

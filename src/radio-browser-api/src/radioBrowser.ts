5393f64b6d52f437072dd4888d67eee4ea647197
Rolandas Siksnelis <74502149+RolandasSix@users.noreply.github.com>
2022-05-12 18:32:14 +0300

    url
    apkeitimai

--- src/radio-browser-api/src/radioBrowser.ts
+++ src/radio-browser-api/src/radioBrowser.ts
@@ -49,10 +49,10 @@ export class RadioBrowserApi {
 
     // temporary fix for https cert error when in frontend
     // hardcode the server
-    // https://github.com/segler-alex/radiobrowser-api-rust/issues/122
-    // if (typeof window !== 'undefined') {
-    //   return [{ ip: '45.77.62.161', name: 'fr1.api.radio-browser.info' }]
-    // }
+    'https://github.com/segler-alex/radiobrowser-api-rust/issues/122'
+    if (typeof window !== 'undefined') {
+    return [{ ip: '45.77.62.161', name: 'fr1.api.radio-browser.info' }]
+    }
     const response = await fetch(
       // this should be https when the above issue is resolved
       'https://all.api.radio-browser.info/json/servers',
